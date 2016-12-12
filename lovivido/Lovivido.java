bf3a97b99f684bd7ac24723ca3a1ca5ccf12ee4d
Adrián Silva <adri4silva@gmail.com>
2016-12-12 08:56:52 +0100

    Formateo y cambio de nombre a las variables

---
+++ lovivido/Lovivido.java
@@ -0,0 +1,34 @@
+/*
+ * Examen Refactorizacion
+ * 
+ */
+package lovivido;
+
+import javax.swing.JOptionPane;
+
+public class Lovivido{
+
+    public static void main(String[] args) {
+        String nombre;
+        String edad;
+
+        int mes, dia, hora, años;
+
+        nombre = JOptionPane.showInputDialog("Escriba su nombre: ");
+        edad = JOptionPane.showInputDialog("Escriba su edad: ");
+
+        años = Integer.parseInt(edad);
+
+        mes = (años * 12);
+        dia = (años * 365);
+        hora = (dia * 24);
+
+        JOptionPane.showMessageDialog(null, "Meses: " + mes, "Numero de meses vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Días: " + dia, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "Horas: " + hora, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
+
+        System.exit(0);
+    }
+}
